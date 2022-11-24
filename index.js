71189e8cff99bc915545f61a74bad0051045ce61
sascha radojkovic <scha13@gmx.de>
2022-11-24 11:46:29 +0100

    modified different files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const button1 = document.querySelector('[data-js="bookmark1"]');
 button1.addEventListener("click", () => {
+  console.log("test");
+  button1.classList.remove("bookmark");
   button1.classList.toggle("active");
+
   console.log("test");
 });
