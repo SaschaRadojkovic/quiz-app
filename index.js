139822665f2ec5d72474717760ca46a63af42e6a
sascha radojkovic <scha13@gmx.de>
2022-12-02 00:15:17 +0100

    adjust form.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const form = document.querySelector('[data-js="form"]');
-const newQuestion = document.querySelector('[data-js="new-question"]');
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-  console.log(data);
-
-  event.target.elements.yourquestion.focus();
-  const quest = document.createElement("li");
-  newQuestion.append(quest);
-  const answer = document.createElement("li");
-  newQuestion.append(answer);
-  const tag = document.createElement("li");
-  newQuestion.append(tag);
-  quest.textContent = data.yourquestion;
-  answer.textContent = data.youranswer;
-  tag.textContent = data.taginput;
-  event.target.reset();
-});
-
 // console.clear();
 
 // const button1 = document.querySelector('[data-js="bookmark1"]');
@@ -33,13 +11,21 @@ form.addEventListener("submit", (event) => {
 const answer1 = document.querySelector('[data-js="showanswer1"]');
 console.log("answer1", answer1);
 const toastContainer = document.querySelector('[data-js="toast-container"]');
-const answerText = document.querySelector('[data-js="answer-text"]');
+const newAnswer = document.querySelector('[data-js="answer-text"]');
+
+// newAnswer = document.createElement("li");
+// newAnswer.classList.add("answer-test");
+// newAnswer.textContent = "hier deine Antwort";
+// newAnswer.hidden = true;
+// toastContainer.append(newAnswer);
 
 answer1.addEventListener("click", () => {
-  const newAnswer = document.createElement("li");
-  newAnswer.classList.add("answer-test");
-  newAnswer.textContent = "hier deine Antwort";
-  toastContainer.append(newAnswer);
+  newAnswer.hidden = !newAnswer.hidden;
+  if (newAnswer.hidden) {
+    answer1.textContent = "Show Answer";
+  } else {
+    answer1.textContent = "Hide Answer";
+  }
 });
 
 // answer1.addEventListener("click", () => {
