0124befc239ddd8a6ba6a7a1e7d3d035c6466009
sascha radojkovic <scha13@gmx.de>
2022-12-01 14:34:13 +0100

    return to recap status

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+// const button1 = document.querySelector('[data-js="bookmark1"]');
+// button1.addEventListener("click", () => {
+//   console.log("test");
+//   button1.classList.remove("bookmark");
+//   button1.classList.toggle("active");
+
+//   console.log("test");
+// });
+const answer1 = document.querySelector('[data-js="showanswer1"]');
+answer1.addEventListener("click", () => {
+  //   if ( {
+  //     output.textContent = "you are teen";
+  //   } else {
+  //     output.textContent = "you are not a teen";
+  //   }
+});
+
 const button1 = document.querySelector('[data-js="bookmark1"]');
 button1.addEventListener("click", () => {
-  console.log("test");
-  button1.classList.remove("bookmark");
   button1.classList.toggle("active");
-
-  console.log("test");
+});
+const button2 = document.querySelector('[data-js="bookmark2"]');
+button2.addEventListener("click", () => {
+  button2.classList.toggle("active");
+});
+const button3 = document.querySelector('[data-js="bookmark3"]');
+button3.addEventListener("click", () => {
+  button3.classList.toggle("active");
+});
+const button4 = document.querySelector('[data-js="bookmark4"]');
+button4.addEventListener("click", () => {
+  button4.classList.toggle("active");
 });
